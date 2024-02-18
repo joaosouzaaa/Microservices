ee17846ff868d89ffea5c2b7de003283a7b99cc1
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-18 19:06:44 -0300

    Feat: Adding test builders.

---
+++ PatientService.UnitTests/TestBuilders/ContractsBuilder.cs
@@ -0,0 +1,13 @@
+﻿using PatientService.API.Contracts;
+
+namespace PatientService.UnitTests.TestBuilders;
+public sealed class ContractsBuilder
+{
+    public static ContractsBuilder NewObject() =>
+        new();
+
+    public AppointmentTimeCreatedEvent AppointmentTimeCreatedEventBuild() =>
+        new(DateTime.Now,
+            12,
+            9);
+}
