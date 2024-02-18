7cb754fbccadcc75bee54d9a1f3834247a71ba6d
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-18 18:06:52 -0300

    Feat: Adding connection string factory.

---
+++ PatientService.API/Factories/ConnectionStringFactory.cs
@@ -0,0 +1,13 @@
+﻿using PatientService.API.Factories;
+
+namespace PatientService.API.Factories;
+public static class ConnectionStringFactory
+{
+    public static string GetConnectionString(this IConfiguration configuration)
+    {
+        if (Environment.GetEnvironmentVariable("DOCKER_ENVIROMENT") == "PATIENT_DOCKER_ENV")
+            return configuration.GetConnectionString("ContainerConnection")!;
+
+        return configuration.GetConnectionString("LocalConnection")!;
+    }
+}
