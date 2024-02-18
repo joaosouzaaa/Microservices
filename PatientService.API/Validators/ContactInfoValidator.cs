4ebc98480dac071cf2411d8469fe7216b380a7ea
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-18 19:00:46 -0300

    Feat: Adding validators.

---
+++ PatientService.API/Validators/ContactInfoValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using PatientService.API.Entities;
+using PatientService.API.Enums;
+using PatientService.API.Extensions;
+
+namespace PatientService.API.Validators;
+public sealed class ContactInfoValidator : AbstractValidator<ContactInfo>
+{
+    public ContactInfoValidator()
+    {
+        RuleFor(c => c.PhoneNumber).Length(11)
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("Phone number", "11"));
+
+        RuleFor(c => c.Email).EmailAddress().Length(1, 100)
+            .WithMessage(EMessage.InvalidFormat.Description().FormatTo("Email"));
+    }
+}
---
+++ PatientService.API/Validators/PatientClientValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using PatientService.API.Entities;
+using PatientService.API.Enums;
+using PatientService.API.Extensions;
+
+namespace PatientService.API.Validators;
+public sealed class PatientClientValidator : AbstractValidator<PatientClient>
+{
+    public PatientClientValidator()
+    {
+        RuleFor(p => p.ContactInfo).SetValidator(new ContactInfoValidator());
+
+        RuleFor(p => p.Name).Length(1, 100).NotEmpty()
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("Name", "1 to 100"));
+
+        RuleFor(p => p.Address).Length(1, 200).NotEmpty()
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("Address", "1 to 200"));
+    }
+}
