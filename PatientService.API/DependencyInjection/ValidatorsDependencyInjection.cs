5ffd64e3e591e326f21ee8f4187ce6489e6ef355
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-18 19:10:43 -0300

    Feat: Adding validators dependency injection.

---
+++ PatientService.API/DependencyInjection/ValidatorsDependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using PatientService.API.Entities;
+using PatientService.API.Validators;
+
+namespace PatientService.API.DependencyInjection;
+public static class ValidatorsDependencyInjection
+{
+    public static void AddValidatorsDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<ContactInfo>, ContactInfoValidator>();
+        services.AddScoped<IValidator<PatientClient>, PatientClientValidator>();
+    }
+}
