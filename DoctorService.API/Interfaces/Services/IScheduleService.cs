fa88af23b4d48e7db44c4f626e37f532717e3d44
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-18 16:59:24 -0300

    Feat: Adding service interfaces and base service

---
+++ DoctorService.API/Interfaces/Services/IScheduleService.cs
@@ -0,0 +1,7 @@
+﻿using DoctorService.API.Contracts;
+
+namespace DoctorService.API.Interfaces.Services;
+public interface IScheduleService
+{
+    Task AddAsync(AppointmentTimeCreatedEvent appointmentTimeCreatedEvent);
+}
---
+++ DoctorService.API/Interfaces/Services/ISpecialityServiceFacade.cs
@@ -0,0 +1,7 @@
+﻿using DoctorService.API.Entities;
+
+namespace DoctorService.API.Interfaces.Services;
+public interface ISpecialityServiceFacade
+{
+    Task<Speciality?> GetByIdReturnsDomainObjectAsync(int id);
+}
