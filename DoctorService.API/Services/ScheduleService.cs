d1972221e7d8f8127c36767045d4c2d962cc1372
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-18 17:08:17 -0300

    Feat: Adding services classes.

---
+++ DoctorService.API/Services/ScheduleService.cs
@@ -0,0 +1,15 @@
+﻿using DoctorService.API.Contracts;
+using DoctorService.API.Interfaces.Mappers;
+using DoctorService.API.Interfaces.Repositories;
+using DoctorService.API.Interfaces.Services;
+
+namespace DoctorService.API.Services;
+public sealed class ScheduleService(IScheduleRepository scheduleRepository, IScheduleMapper scheduleMapper) : IScheduleService
+{
+    public async Task AddAsync(AppointmentTimeCreatedEvent appointmentTimeCreatedEvent)
+    {
+        var schedule = scheduleMapper.AppointmentTimeCreatedEventToDomain(appointmentTimeCreatedEvent);
+
+        await scheduleRepository.AddAsync(schedule);
+    }
+}
