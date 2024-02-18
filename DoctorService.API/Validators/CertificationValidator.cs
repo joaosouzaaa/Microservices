a20c00d177974c1342473a77951411543f0cd1bd
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-18 16:22:36 -0300

    Feat: Addings validators and error enum.

---
+++ DoctorService.API/Validators/CertificationValidator.cs
@@ -0,0 +1,13 @@
+﻿using DoctorService.API.Entities;
+using DoctorService.API.Enums;
+using FluentValidation;
+
+namespace DoctorService.API.Validators;
+public sealed class CertificationValidator : AbstractValidator<Certification>
+{
+    public CertificationValidator()
+    {
+        RuleFor(c => c.LicenseNumber).Length(20)
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("License Number", "20"));
+    }
+}
---
+++ DoctorService.API/Validators/SpecialityValidator.cs
@@ -0,0 +1,13 @@
+﻿using DoctorService.API.Entities;
+using DoctorService.API.Enums;
+using FluentValidation;
+
+namespace DoctorService.API.Validators;
+public sealed class SpecialityValidator : AbstractValidator<Speciality>
+{
+    public SpecialityValidator()
+    {
+        RuleFor(s => s.Name).NotEmpty().Length(1, 100)
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("Name", "1 to 100"));
+    }
+}
