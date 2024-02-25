fe77a1b377a6a512b3576a3e2a88a55ec42a5d6c
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-24 21:38:42 -0300

    Tests: Extension tests.

---
+++ AppointmentService.UnitTests/ExtensionsTests/MessageExtensionTests.cs
@@ -0,0 +1,19 @@
+﻿using AppointmentService.API.Enums;
+using AppointmentService.API.Extensions;
+
+namespace AppointmentService.UnitTests.ExtensionsTests;
+public sealed class MessageExtensionTests
+{
+    [Fact]
+    public void Description_Equals_AsIntended()
+    {
+        // A
+        var messageToGetDescription = EMessage.GreaterThan;
+
+        // A
+        var messageDescription = messageToGetDescription.Description();
+
+        // A
+        Assert.Equal("{0} has to be greater than {1}.", messageDescription);
+    }
+}
