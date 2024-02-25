c9fc5f8f4f6c12c5b5debac546ef570943e3bd9b
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-24 21:37:32 -0300

    Feat: Adding extensions and appointment validator.

--- AppointmentService.API/DependencyInjection/DependencyInjectionHandler.cs
+++ AppointmentService.API/DependencyInjection/DependencyInjectionHandler.cs
@@ -22,5 +22,6 @@ public static class DependencyInjectionHandler
         services.AddRepositoriesDependencyInjection();
         services.AddPublishersDependencyInjection();
         services.AddMappersDependencyInjection();
+        services.AddValidatorsDependencyInjection();
     }
 }
---
+++ AppointmentService.API/DependencyInjection/ValidatorsDependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using AppointmentService.API.Entities;
+using AppointmentService.API.Validators;
+using FluentValidation;
+
+namespace AppointmentService.API.DependencyInjection;
+public static class ValidatorsDependencyInjection
+{
+    public static void AddValidatorsDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<AppointmentTime>, AppointmentTimeValidator>();
+    }
+}
