545a511baa0cd4ac7d467e2089d4a43fdeaaef06
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-24 21:22:10 -0300

    Feat: Adding rabbit mq on docker compose and options in appointment service.

---
+++ AppointmentService.API/DependencyInjection/OptionsDependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using AppointmentService.API.Constants;
+using AppointmentService.API.Options;
+
+namespace AppointmentService.API.DependencyInjection;
+public static class OptionsDependencyInjection
+{
+    public static void AddOptionsDependencyInjection(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<RabbitMQCredentialsOptions>(options => configuration.GetSection(OptionsConstants.RabbitMQCredentialsSection).Bind(options));
+    }
+}
