4bd452b4a28edfeef2a7367c75935b773514f176
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-24 21:49:35 -0300

    Feat: Adding appointment controller.

---
+++ AppointmentService.API/Controllers/AppointmentTimeController.cs
@@ -0,0 +1,17 @@
+﻿using AppointmentService.API.DataTransferObjects.Appointment;
+using AppointmentService.API.Interfaces.Services;
+using AppointmentService.API.Settings.NotificationSettings;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentService.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public sealed class AppointmentTimeController(IAppointmentTimeService appointmentTimeService) : ControllerBase
+{
+    [HttpPost("add")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(bool))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(List<Notification>))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public Task<bool> AddAsync([FromBody] AppointmentTimeSave appointmentTimeSave) =>
+        appointmentTimeService.AddAsync(appointmentTimeSave);
+}
